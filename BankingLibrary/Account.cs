2ce02f45ba8abd200de2e8383bdbf72607498c3a
max-student <max-student@maxtrain.com>
2020-02-12 18:04:55 -0500

    Amount Assignment

--- BankingLibrary/Account.cs
+++ BankingLibrary/Account.cs
@@ -3,17 +3,25 @@
 namespace BankingLibrary {
     public class Account {
 
-        public decimal Balance { get; set; }
-        public string AcctNbr { get; set; }
+        public decimal Balance { get; set; } //cannot deposit or withdraw anything < 1 or withdraw any more than what is in the balance
+        public int AcctNbr { get; set; } //acctnbr needs a unique value
         public string Description { get; set; }
+        private int NextNbr = 0;
 
         public void Deposit(decimal amount) {
-            Balance += amount;
+            if (amount > 0) {
+                Balance += amount;
+            } else{
+                Console.WriteLine("Cannot Deposit");
+            }
         }
 
         public void Withdraw(decimal amount) {
-           Balance -= amount;
-
+            if (amount > 0 && amount <= Balance) {
+                Balance -= amount;
+            } else {
+                Console.WriteLine("Cannot Withdraw");
+            }
         }
 
         public void Transfer(decimal amount, Account toAccount, Account fromAccount){
@@ -22,8 +30,8 @@ namespace BankingLibrary {
          }
 
         public Account() {
-
-        }
+            AcctNbr = NextNbr += 1;
+            }   
 
     }
 }
